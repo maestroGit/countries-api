82c473cfe036a1d2d90b6d2ee063d5ba8072d630
walkexperience <walkexpereince@gmail.com>
2020-11-12 13:28:12 +0100

    Statement on a detected navigator

--- js/search.js
+++ js/search.js
@@ -1,27 +1,36 @@
-const formSearch = document.getElementById('formSearch');
-const inputForm = document.getElementById('inputTexto');
+const formSearch = document.getElementById("formSearch");
+const inputForm = document.getElementById("inputTexto");
+const device = navigator.userAgent;
 
 function blurInput() {
-    document.getElementById('inputTexto').blur();
-  }
-
-const searchData = data => {
-    formSearch.addEventListener('keyup',e => {
-        e.preventDefault()
-        const inputValue = inputForm.value.toLowerCase();
-        //console.log(inputValue);
-
-        // array con el fitro por busqueda inputValue
-        //método filter crea una nueva array con elementos bajo un criterio dado de la array existente:
-        const arrayMatchInputValue = data.filter( elements => {
-            const namesCountries = elements.name.toLowerCase();
-            //console.log(namesCountries);
-            if (namesCountries.indexOf(inputValue) !== -1) {
-               return elements;
-            }
-        })
-        //console.log(arrayMatchInputValue);
-        showCountries(arrayMatchInputValue);
-        setTimeout(blurInput, 4000);
-    })
+  document.getElementById("inputTexto").blur();
 }
+
+const searchData = (data) => {
+  formSearch.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    const inputValue = inputForm.value.toLowerCase();
+    //console.log(inputValue);
+
+    // array con el fitro por busqueda inputValue
+    //método filter crea una nueva array con elementos bajo un criterio dado de la array existente:
+    const arrayMatchInputValue = data.filter((elements) => {
+      const namesCountries = elements.name.toLowerCase();
+      //console.log(namesCountries);
+      if (namesCountries.indexOf(inputValue) !== -1) {
+        return elements;
+      }
+    });
+    
+    showCountries(arrayMatchInputValue);
+    if (
+      device.match(/Android/i) ||
+      device.match(/webOS/i) ||
+      device.match(/iPhone/i) ||
+      device.match(/iPod/i) ||
+      device.match(/iPad/i)
+    ) {
+      setTimeout(blurInput, 4000);
+    }
+  });
+};
